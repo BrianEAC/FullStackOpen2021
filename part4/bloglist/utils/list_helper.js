7eddf70453a5683939b3ef724d4def21285bee2c
BrianEAC <brianatariaceraldi@gmail.com>
2021-08-30 13:58:42 -0300

    exercises up to 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,12 +1,18 @@
-const dummy = blogs => 1    
+const dummy = (blogs) => 1;
 
-const totalLikes = blogs => {
-    total = 0
-    blogs.forEach(blog => total += blog.likes)
-    return total
-}
+const totalLikes = (blogs) => {
+  total = 0;
+  blogs.forEach((blog) => (total += blog.likes));
+  return total;
+};
+
+const favBlog = (blogs) => {
+  if (blogs.length > 0)
+    return blogs.reduce((max, blog) => (max.likes > blog.likes ? max : blog));
+};
 
 module.exports = {
-    dummy,
-    totalLikes
-}
+  dummy,
+  totalLikes,
+  favBlog,
+};
